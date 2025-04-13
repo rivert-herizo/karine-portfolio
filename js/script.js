089c47f02655813c29097e7368058e477281c0bd
Hello <totoroherizo@gmail.com>
2025-04-13 18:02:08 +0300

    modal fix

--- js/script.js
+++ js/script.js
@@ -202,16 +202,6 @@ service_nav.forEach((nav, index) => {
     });
 });
 
-// Handle URL tab parameter
-const params = new URLSearchParams(window.location.search);
-const tabIndex = parseInt(params.get('tab'));
-
-if (!isNaN(tabIndex) && service_nav[tabIndex]) {
-    service_nav[tabIndex].click();
-} else {
-    service_nav[0].click(); // default to first tab
-}
-
 const boxes = document.querySelectorAll('.ghost-writing-box');
 const modal = document.getElementById('modal');
 const modalTitle = document.querySelector('.modal-main-title');
@@ -244,4 +234,12 @@ window.addEventListener('click', (e) => {
     }
 });
 
+// Handle URL tab parameter
+const params = new URLSearchParams(window.location.search);
+const tabIndex = parseInt(params.get('tab'));
 
+if (!isNaN(tabIndex) && service_nav[tabIndex]) {
+    service_nav[tabIndex].click();
+} else {
+    service_nav[0].click(); // default to first tab
+}
